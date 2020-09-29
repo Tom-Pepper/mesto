8ab5f3097ee60f42c9c9d969a52c4e88ff7ae653
Artem Kondratyev <walinor.88@gmail.com>
2020-09-29 23:07:04 +0300

    style: исправить отображение кнопок 'лайк' в адаптиве

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,16 @@ let buttonEditProfile = document.querySelector(".profile__edit-button");
 let buttonClosePopup = document.querySelector(".popup__close");
 let popup = document.querySelector(".popup");
 
-function popupToggle()  {
+const popupToggle = () =>  {
   popup.classList.toggle("popup_is-opened");
+}
+
+const closePopupLayerClick = (event) => {
+  if (event.target === event.currentTarget) {
+    popupToggle();
+  }
 }
 
 buttonEditProfile.addEventListener('click', popupToggle);
 buttonClosePopup.addEventListener('click', popupToggle);
+popup.addEventListener('click', closePopupLayerClick);
