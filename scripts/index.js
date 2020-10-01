5bac9276313f4ba71c7db40c4e1e2c0cb78629b6
Artem Kondratyev <walinor.88@gmail.com>
2020-10-01 19:41:59 +0300

    refactor: изменение адаптива pop-up, правки замечаний, добавление шрифтов woff

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 // Объявление переменных
 let buttonEditProfile = document.querySelector(".profile__edit-button");
-let buttonClosePopup = document.querySelector(".popup__close");
 let popup = document.querySelector(".popup");
+let buttonClosePopup = popup.querySelector(".popup__close");
 
-let nameInput = document.querySelector(".popup__name");
-let jobInput = document.querySelector(".popup__job");
+let formElement = popup.querySelector(".popup__form");
+let nameInput = popup.querySelector(".popup__name");
+let jobInput = popup.querySelector(".popup__job");
 let nameToEdit = document.querySelector(".profile__info-name");
 let jobToEdit = document.querySelector(".profile__info-job");
 // ------
 
 // Открытие и закрытие поп-апа
 const popupOpen = () =>  {
-  popup.classList.add("popup_is-opened");
   nameInput.value = nameToEdit.textContent;
   jobInput.value = jobToEdit.textContent;
+  popup.classList.add("popup_is-opened");
 }
 
 const popupClose = () =>  {
@@ -31,11 +32,13 @@ buttonClosePopup.addEventListener('click', popupClose);
 popup.addEventListener('click', closePopupLayerClick);
 // ------
 
-let formElement = document.querySelector(".popup__form");
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameToEdit.textContent = nameInput.value;
   jobToEdit.textContent = jobInput.value;
   popupClose();
+
+  nameInput.value ='';
+  jobInput.value ='';
 }
 formElement.addEventListener('submit', formSubmitHandler);
