b63434d21d1e1b18f65b9715dddff39cb8a4b5ff
Artem Kondratyev <walinor.88@gmail.com>
2020-10-09 15:48:00 +0300

    feat: реализовать поп-ап добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -26,13 +26,22 @@ const initialCards = [
   }
 ];
 
-const elemetnsTemplate = document.querySelector('.elements__template').content;
-const elementsPoition = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.elements__template').content;
+const elementPosition = document.querySelector('.elements');
+
+const renderCards = () => {
+  initialCards.map(element => {
+
+  })
+}
 
 const buttonEditProfile = document.querySelector(".profile__edit-button");
+const buttonAddPlace = document.querySelector(".profile__add-button");
 const popup = document.querySelector(".popup");
+const addPlacePopup = document.querySelector(".popup-new-place");
 const buttonClosePopup = popup.querySelector(".popup__close");
-const likeButton = document.querySelector('.element__like-button');
+const buttonCloseAddPlacePopup = document.querySelector(".popup-new-place__close");
+const likeButton = document.querySelector(".element__like-button");
 
 const formElement = popup.querySelector(".popup__form");
 const nameInput = popup.querySelector(".popup__name");
@@ -42,33 +51,31 @@ const jobToEdit = document.querySelector(".profile__info-job");
 
 
 //Открытие и закрытие поп-апа редактировния профиля
-const popupOpen = () =>  {
+const profilePopupOpen = () =>  {
   nameInput.value = nameToEdit.textContent;
   jobInput.value = jobToEdit.textContent;
   popup.classList.add("popup_is-opened");
 }
 
-const popupClose = () =>  {
+const profilePopupClose = () =>  {
   popup.classList.remove("popup_is-opened");
 }
 
 const closePopupLayerClick = (event) => {
   if (event.target === event.currentTarget) {
-    popupClose();
+    profilePopupClose();
   }
 }
-
-buttonEditProfile.addEventListener('click', popupOpen);
-buttonClosePopup.addEventListener('click', popupClose);
+buttonEditProfile.addEventListener('click', profilePopupOpen);
+buttonClosePopup.addEventListener('click', profilePopupClose);
 popup.addEventListener('click', closePopupLayerClick);
 //------
-
 //Изменение имени и профессии профиля
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameToEdit.textContent = nameInput.value;
   jobToEdit.textContent = jobInput.value;
-  popupClose();
+  profilePopupClose();
 
   nameInput.value ='';
   jobInput.value ='';
@@ -76,7 +83,25 @@ function formSubmitHandler (evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 //------
 
+//Смена цвета сердечка при клике на лайк
 likeButton.addEventListener('click', function (evt) {
   const clickTarget = evt.target;
   clickTarget.classList.toggle('element__like-button_active');
 });
+
+//Открытие и закрытие поп-апа добавления новых карточек
+const addPlacePopupOpen = () => {
+  addPlacePopup.classList.add("popup_is-opened");
+}
+const addPlacePopupClose = () => {
+  addPlacePopup.classList.remove("popup_is-opened");
+}
+const addPLacePopupLayerClickClosure = (event) => {
+  if (event.target === event.currentTarget) {
+    addPlacePopupClose();
+  }
+}
+buttonAddPlace.addEventListener('click', addPlacePopupOpen);
+buttonCloseAddPlacePopup.addEventListener('click', addPlacePopupClose);
+addPlacePopup.addEventListener('click', addPLacePopupLayerClickClosure);
+//------
